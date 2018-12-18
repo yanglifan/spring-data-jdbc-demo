4b005fb904865e1ec4e230fea982f76a2bd0f532
Yang Lifan <yanglifan@qiyi.com>
2018-12-18 14:50:17 +0800

    自定义表名

--- src/main/java/com/github/yanglifan/springdatajdbcdemo/CustomerRepository.java
+++ src/main/java/com/github/yanglifan/springdatajdbcdemo/CustomerRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
-    @Query("select * from Customer c where c.name = :name")
+    @Query("select * from t_customers c where c.name = :name")
     Customer findByName(@Param("name") String name);
 }
