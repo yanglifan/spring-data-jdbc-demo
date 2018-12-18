4586f3879c7f547ca147414fa182cd6f6dc59812
Yang Lifan <yanglifan@qiyi.com>
2018-12-18 13:44:08 +0800

    自定义表名

--- src/main/java/com/github/yanglifan/springdatajdbcdemo/Customer.java
+++ src/main/java/com/github/yanglifan/springdatajdbcdemo/Customer.java
@@ -1,9 +1,11 @@
 package com.github.yanglifan.springdatajdbcdemo;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDate;
 
+@Table("t_customers")
 public class Customer {
     @Id
     Long id;
